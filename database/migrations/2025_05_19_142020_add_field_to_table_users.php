77262057c9677c0ae49f5c8b600597054ad4d676
onesarumaha <onesarumaha10@gmail.com>
2025-05-19 21:33:14 +0700

    aplikasi evotting

---
+++ database/migrations/2025_05_19_142020_add_field_to_table_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('nis')->nullable()->after('name');
+            $table->string('username')->nullable()->after('name');
+            $table->string('jk')->nullable()->after('email');
+            $table->string('kelas')->nullable()->after('email');
+            $table->string('role')->nullable()->after('email');
+            $table->text('alamat')->nullable()->after('email');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['username', 'role', 'nis', 'jk', 'kelas', 'alamat']);
+        });
+    }
+};
---
+++ resources/views/layouts/_sidebar.blade.php
@@ -0,0 +1,67 @@
+<div class="sidebar" data-background-color="dark">
+    <div class="sidebar-logo">
+        <!-- Logo Header -->
+        <div class="logo-header" data-background-color="dark">
+        <a href="index.html" style="font-size: 20px; color:white; padding-left:30px;" class="logo"><b>E-Votting</b> </a>
+        <div class="nav-toggle">
+            <button class="btn btn-toggle toggle-sidebar">
+            <i class="gg-menu-right"></i>
+            </button>
+            <button class="btn btn-toggle sidenav-toggler">
+            <i class="gg-menu-left"></i>
+            </button>
+        </div>
+        <button class="topbar-toggler more">
+            <i class="gg-more-vertical-alt"></i>
+        </button>
+        </div>
+        <!-- End Logo Header -->
+    </div>
+    <div class="sidebar-wrapper scrollbar scrollbar-inner">
+        <div class="sidebar-content">
+        <ul class="nav nav-secondary">
+            <li class="nav-item active">
+                <a
+                    data-bs-toggle="collapse"
+                    href="#dashboard"
+                    class="collapsed"
+                    aria-expanded="false"
+                >
+                    <i class="fas fa-home"></i>
+                    <p>Dashboard</p>
+                </a>
+            </li>
+            <li class="nav-section">
+            <span class="sidebar-mini-icon">
+                <i class="fa fa-ellipsis-h"></i>
+            </span>
+            <h4 class="text-section">Menu</h4>
+            </li>        
+            <li class="nav-item">
+                <a href="widgets.html">
+                    <i class="fas fa-user-alt"></i>
+                    <p>Data Siswa</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="widgets.html">
+                    <i class="fas fa-user-friends"></i>
+                    <p>Data Kandidat</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="widgets.html">
+                    <i class="fas fa-th"></i>
+                    <p>Laporan</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="widgets.html">
+                    <i class="fas fa-sign-out-alt"></i>
+                    <p>Keluar</p>
+                </a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</div>
